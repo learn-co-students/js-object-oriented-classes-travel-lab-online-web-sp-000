350caab5a77ef7626c42be23f71a01dda09a9601
Jonathan Tuzman <tuzmusic@gmail.com>
2019-02-13 11:52:00 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
+class Driver { 
+  
+  constructor(name, dayJoined) {
+    this.name = name
+    this.startDate = new Date(dayJoined)
+  }
+
+  yearsExperienceFromBeginningOf(year) {
+    let firstOfYear = new Date(year,0,1)
+    let yearsSince = Math.floor((firstOfYear - this.startDate) / 31536000000)
+    return yearsSince
+  }
+}
+
+class Route {
+  constructor(beginningLocation, endingLocation) {
+    this.beginningLocation = beginningLocation
+    this.endingLocation = endingLocation
+  }
+
+  blocksTravelled() {
+    let eastWest = [
+      '1st Avenue',
+      '2nd Avenue',
+      '3rd Avenue',
+      'Lexington Avenue',
+      'Park',
+      'Madison Avenue',
+      '5th Avenue'
+    ];
+
+    let streets = Math.abs(this.beginningLocation.vertical - this.endingLocation.vertical)
+    let avenues = Math.abs(eastWest.indexOf(this.beginningLocation.horizontal) - eastWest.indexOf(this.endingLocation.horizontal))
+
+    return streets + avenues
+  }
+
+  estimatedTime(peak = false) {
+    return Math.floor(this.blocksTravelled() / (peak ? 2 : 3))
+  }
+  
+}
 
