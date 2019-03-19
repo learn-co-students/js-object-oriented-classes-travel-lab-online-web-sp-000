cc9a139651db471aa126d42d712bdaa92f4cc861
Miranda Bonilla <bonillamiranda3@gmail.com>
2019-03-19 01:36:11 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
+class Driver {
+  constructor(name, startDate) {
+    this.name = name
+    this.startDate = new Date(startDate);
+  }
 
+    yearsExperienceFromBeginningOf(year) {
+    return year - this.startDate.getFullYear();
+  }
+}
+
+class Route {
+  constructor(start, end) {
+    this.start = start;
+    this.end = end;
+  }
+
+  blocksTravelled() {
+    const distance = Math.abs(this.end.vertical - this.start.vertical);
+    const blocks = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue',
+    'Park', 'Madison Avenue', '5th Avenue'];
+    const travelled = Math.abs(blocks.indexOf(this.start.horizontal) - blocks.indexOf(this.end.horizontal));
+    return distance + travelled;
+  }
+
+  estimatedTime(peakHour) {
+    if(peakHour === undefined) {
+      return this.blocksTravelled()/3;
+    }
+    else {
+      return this.blocksTravelled()/2;
+    }
+  }
+}
