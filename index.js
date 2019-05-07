fbaab5c7f1767b05f7d272096c4bb7637fa68dd9
Cookie McCormick <alisa.cookie.mccormick@gmail.com>
2019-05-07 15:14:16 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
+class Driver {
+  constructor(name, startDate) {
+    this.name = name;
+    this.startDate = new Date(startDate);
+  }
 
+  yearsExperienceFromBeginningOf(year) {
+    return year - this.startDate.getFullYear();
+  }
+}
+
+class Route {
+  constructor( beginningLocation, endingLocation ) {
+    this.beginningLocation = beginningLocation;
+    this.endingLocation = endingLocation;
+  }
+
+  blocksTravelled() {
+    let eastWest = [
+      '1st Avenue',
+      '2nd Avenue',
+      '3rd Avenue',
+      'Lexington Avenue',
+      'Park',
+      'Madison Avenue',
+      '5th Avenue'
+    ];
+
+    let beginningStreet = this.beginningLocation.vertical;
+    let endingStreet = this.endingLocation.vertical;
+    let beginningAvenue = eastWest.indexOf(this.beginningLocation.horizontal);
+    let endingAvenue = eastWest.indexOf(this.endingLocation.horizontal);
+
+    let verticalDistance = Math.abs(beginningStreet - endingStreet);
+    let horizontalDistance = Math.abs(beginningAvenue - endingAvenue);
+
+    return verticalDistance + horizontalDistance;
+  }
+
+  estimatedTime(peak) {
+    if (peak) {
+      return this.blocksTravelled() / 2;
+    } else {
+      return this.blocksTravelled()/ 3;
+    }
+  }
+}
