c155fbe749b0b0d303ca3d6214b7bd9f8f3a9255
Thomas Kunkel <thomas.boyd.kunkel@gmail.com>
2019-07-06 09:57:20 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
+class Driver {
+  constructor(name, startDate) {
+    [this.name, this.startDate] = [name, new Date(startDate)]
+  }
+  startDate() {
+    return this.startDate
+  }
+  yearsExperienceFromBeginningOf(year) {
+    return (year - this.startDate.getFullYear())
+  }
+}
 
+class Route {
+  constructor(beginningLocation, endingLocation) {
+    [this.beginningLocation, this.endingLocation] = [beginningLocation, endingLocation]
+  }
+  blocksTravelled() {
+    let eastWest = [
+      '1st Avenue',
+      '2nd Avenue',
+      '3rd Avenue',
+      'Lexington Avenue',
+      'Park',
+      'Madison Avenue',
+      '5th Avenue'
+    ];
+    const horizontalDistance = Math.abs(eastWest.indexOf(this.beginningLocation["horizontal"]) - eastWest.indexOf(this.endingLocation["horizontal"]))
+    const verticalDistance = Math.abs(this.beginningLocation["vertical"] - this.endingLocation["vertical"])
+    return horizontalDistance + verticalDistance
+  }
+  estimatedTime(peakHours) {
+    if (typeof peakHours == 'undefined') {
+      return this.blocksTravelled() / 3
+    } else {
+      return this.blocksTravelled() / 2
+    }
+  }
+}
