4a6820347ae3fb32b0d02a0b60b95a718fe83f5b
blainelawson <blaine.lawson@gmail.com>
2019-07-25 11:12:22 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
+class Driver {
+    constructor (name, startDate) {
+        this.name = name;
+        this.startDate = new Date(startDate);
+    }
 
+    yearsExperienceFromBeginningOf(endDate) {
+        return endDate - (this.startDate.getYear()+1900)
+    }
+}
+
+let eastWest = [
+    '1st Avenue',
+    '2nd Avenue',
+    '3rd Avenue',
+    'Lexington Avenue',
+    'Park',
+    'Madison Avenue',
+    '5th Avenue'
+  ];
+
+class Route {
+    constructor (beginningLocation, endingLocation) {
+        this.beginningLocation = beginningLocation;
+        this.endingLocation = endingLocation;
+    }
+
+    blocksTravelled() {
+        const horizontalBlockA = eastWest.indexOf(this.beginningLocation.horizontal) 
+        const horizontalBlockB = eastWest.indexOf(this.endingLocation.horizontal) 
+        const horizontalDistance = horizontalBlockB - horizontalBlockA
+        const verticalDistance = this.endingLocation.vertical - this.beginningLocation.vertical
+        return horizontalDistance + verticalDistance;
+    }
+
+    estimatedTime(isPeak) {
+        if (isPeak === true) {
+            return this.blocksTravelled() / 2;
+        }else{
+            return this.blocksTravelled() / 3;
+        }
+    }
+}
