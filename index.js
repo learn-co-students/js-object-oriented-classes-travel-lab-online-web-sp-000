d55af2d685ebb98711ca9a1131f20974912ab647
Toni Lamont <tlam212@gmail.com>
2019-08-10 17:30:42 -0400

    Done.

--- index.js
+++ index.js
@@ -25,27 +25,27 @@ class Driver {
   class Route {
 
 
-    constructor( beginningLocation, endingLocation ) {
+    constructor(beginningLocation, endingLocation) {
       this.beginningLocation = beginningLocation;
       this.endingLocation = endingLocation;
     }
 
 
-    avenueToInteger( avenue ) {
-      return eastWest.indexOf( avenue );
+    avenueToInteger(avenue) {
+      return eastWest.indexOf(avenue);
     }
 
     blocksTravelled() {
       let horizontalDistance =
-        this.avenueToInteger( this.endingLocation.horizontal ) -
-        this.avenueToInteger( this.beginningLocation.horizontal );
+        this.avenueToInteger(this.endingLocation.horizontal) -
+        this.avenueToInteger( this.beginningLocation.horizontal);
       let verticalDistance =
         this.endingLocation.vertical - this.beginningLocation.vertical;
-      return Math.abs( horizontalDistance ) + Math.abs( verticalDistance );
+      return Math.abs(horizontalDistance) + Math.abs(verticalDistance);
     }
 
-    estimatedTime( peak ) {
-      if ( peak ) {
+    estimatedTime(peak) {
+      if (peak) {
         return this.blocksTravelled() / 2;
       } else {
         return this.blocksTravelled() / 3;
