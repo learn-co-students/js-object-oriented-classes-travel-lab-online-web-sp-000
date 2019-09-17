ac2425d1968882535f82dab88ca5ed462ff136f7
Andrew Davisson <akdavisson4@gmail.com>
2019-09-17 15:00:46 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
+class Driver {
+  constructor(name, date){
+    this.name = name;
+    this.startDate = new Date(date);
+  }
 
+  yearsExperienceFromBeginningOf(year){
+    
+    return year - this.startDate.getFullYear();
+  }
+}
+
+class Route {
+  constructor(beginningLocation, endingLocation){
+    this.beginningLocation = beginningLocation;
+    this.endingLocation = endingLocation;
+  }
+
+  blocksTravelled(){
+    const eastWest = [
+      '1st Avenue',
+      '2nd Avenue',
+      '3rd Avenue',
+      'Lexington Avenue',
+      'Park',
+      'Madison Avenue',
+      '5th Avenue'
+    ];
+
+    const endingAvenue = eastWest.indexOf(this.endingLocation.horizontal);
+    const beginningAvenue = eastWest.indexOf(this.beginningLocation.horizontal);
+
+    const horizontalBlocks = Math.abs(endingAvenue - beginningAvenue);
+    const verticalBlocks = Math.abs(this.endingLocation.vertical - this.beginningLocation.vertical);
+
+    return horizontalBlocks + verticalBlocks;
+  }
+
+  estimatedTime(peak){
+    if (peak){
+      return this.blocksTravelled() / 2;
+    }else{
+      return this.blocksTravelled() / 3;
+    }
+  }
+}
